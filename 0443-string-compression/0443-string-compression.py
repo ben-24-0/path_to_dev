ba2b48e35a285aea02040fe88ddfc097a5b91218
ben_son <136886788+ben-24-0@users.noreply.github.com>
2026-06-06 18:14:38 +0530

    Time: 0 ms (100%), Space: 19.2 MB (87.91%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,30 +1,19 @@
-class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        i=0
-        j=0
-        nw=""
-        while(i<len(chars)):
-            count=0
-            j=i
-            while(j<len(chars)):
-                if(chars[i]!=chars[j]):
-                    break
-                j+=1
-                count+=1
-            if count >1:
-                nw = nw + chars[i]+ str(count) 
-            else:
-                nw = nw + chars[i]
-            i+=count
-        li = list(nw)
-        n = len(li)
-        print(n)
-        chars[:]= list(nw)
-        return len(chars)
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        nw = ""
+        count = 1
 
-                
-        
+        for i in range(1, len(chars) + 1):
+
+            if i < len(chars) and chars[i] == chars[i - 1]:
+                count += 1
+            else:
+                nw += chars[i - 1]
+
+                if count > 1:
+                    nw += str(count)
+
+                count = 1
+
+        chars[:] = list(nw)
+        return len(chars)
