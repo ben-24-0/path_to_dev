df324a0eb69dcc3e4ec55c2e91b7ce01f1a86d27
ben_son <136886788+ben-24-0@users.noreply.github.com>
2026-06-08 00:34:48 +0530

    my_ver

--- LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ LeetCode/Medium/1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,21 +1,32 @@
 class Solution:
-    def nodays(self,weights,cap):
-        day,load=1,0
-        for i in range(0,len(weights)):
-            if(load+weights[i]>cap):
-                day+=1
-                load=weights[i]
-            else:
-                load+=weights[i]
-        return day
+    # def nodays(self,weights,cap):
+    #     day,load=1,0
+    #     for i in range(0,len(weights)):
+    #         if(load+weights[i]>cap):
+    #             day+=1
+    #             load=weights[i]
+    #         else:
+    #             load+=weights[i]
+    #     return day
     def shipWithinDays(self, weights, days):
         # Your code goes here
-        
+        def nodays(capacity):
+            days_used = 1
+            current_load = 0
+
+            for weight in weights:
+                if current_load + weight > capacity:
+                    days_used += 1
+                    current_load = weight
+                else:
+                    current_load += weight
+
+            return days_used       
         low=max(weights)
         high = sum(weights)
         while(low<=high):
             mid=(low+high)//2
-            nod = self.nodays(weights,mid)
+            nod = nodays(mid)
             if(nod<=days):
                 high=mid-1
             else:
